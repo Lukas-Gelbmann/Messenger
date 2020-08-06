fd15f11f666834a03aa74e9c64f0a9dcf1ea7029
Felix Tröbinger <felix.troebinger@gmail.com>
2020-08-07 00:32:29 +0200

    extract strings to strings.xml and add translation

--- app/src/main/java/at/fhooe/mc/messenger/model/PostParticipantService.java
+++ app/src/main/java/at/fhooe/mc/messenger/model/PostParticipantService.java
@@ -3,13 +3,14 @@ package at.fhooe.mc.messenger.model;
 import retrofit2.Call;
 import retrofit2.http.*;
 
+@SuppressWarnings("HardCodedStringLiteral")
 public interface PostParticipantService {
 
     @Headers({
 
-            "Content-type: application/json"
+            "Content-type: application/json" //NON-NLS
 
     })
-    @POST("/api/participants")
+    @POST("/api/participants") //NON-NLS
     Call<Participant> sendParticipant(@Body Participant participant);
 }
